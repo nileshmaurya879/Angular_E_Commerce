88339f3576b2ee1778cd7dcdcdd3900267ce7acb
nilesh <nileshmaurya879@gmail.com>
2025-05-23 09:32:11 +0530

    Added Header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./Component/header/header.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [HeaderComponent]
 })
 
 export class AppComponent {
