abb24fb6e6119be36ddb724f6436bfbfc6a1a8d4
nilesh <nileshmaurya879@gmail.com>
2025-05-25 15:42:01 +0530

    Added router for home page and seller-auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./Component/header/header.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent,RouterOutlet]
 })
 
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Component/home/home.component';
+import { SellerAuthComponent } from './Component/seller-auth/seller-auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'seller-auth',
+        component: SellerAuthComponent
+    }
+];
